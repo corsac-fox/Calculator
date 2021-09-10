7a381a2c27814f3c30bfa7e975d6baa7fe156c91
corsac-fox <keninfox@gmail.com>
2021-09-10 15:03:53 +0300

    fixed all found bugs

--- Calculator.java
+++ Calculator.java
@@ -3,10 +3,10 @@ package calculator;
 public class Calculator {
 
     Window owner;
-    boolean useMemory, useFirst;
+    boolean useMemory, useFirst, startNew = true;
     double firstOperand, secondOperand, memory;
     int fractionalPart;
-    char operator = '$';
+    char operator;
 
     Calculator (Window owner) {
         this.owner = owner;
@@ -16,16 +16,14 @@ public class Calculator {
         if (fractionalPart > 1) {
             fractionalPart--;
             round();
-        } else secondOperand = (int) secondOperand / 10;
+        } else secondOperand = secondOperand / 10;
         if (fractionalPart == 1) fractionalPart = 0;
-        owner.display.setText(String.valueOf(secondOperand));
     }
 
     void calculate() {
         if (useMemory) {
             if (!useFirst)
                 firstOperand = secondOperand;
-
             secondOperand = memory;
         }
 
@@ -36,28 +34,24 @@ public class Calculator {
             case '-':
                 secondOperand = firstOperand - secondOperand;
                 break;
-            case 0xF7:
-                try
-                {
+            case '÷':
+                try {
                     secondOperand = firstOperand / secondOperand;
-                }
-                catch (ArithmeticException e)
-                {
+                } catch (ArithmeticException e) {
                     if (secondOperand == 0) owner.error();
-                    //else firstOperand = firstOperand.divide(secondOperand, 16, RoundingMode.HALF_UP);
                 }
                 break;
-            case '*':
+            case '×':
                 secondOperand = firstOperand * secondOperand;
                 break;
         }
 
+        owner.check(secondOperand);
+
         firstOperand = 0;
         useMemory = true;
         useFirst = false;
-
-        owner.display.setText(String.valueOf(secondOperand));
-        //if (0 < firstOperand.compareTo(MAX_VALUE)) error();
+        startNew = true;
     }
 
     void cancel() {
@@ -66,14 +60,17 @@ public class Calculator {
         useMemory = false;
         useFirst = false;
         memory = 0;
+        operator = '\u0000';
+        owner.isError = false;
         owner.display.setText("0");
     }
 
     void numberInput(String symbol) {
 
+        if (startNew) cancel();
+
         if (symbol.equals(".")) {
-            if (fractionalPart == 0)
-            {
+            if (fractionalPart == 0) {
                 fractionalPart++;
             }
         } else if (fractionalPart > 0) {
@@ -82,18 +79,16 @@ public class Calculator {
         } else
             secondOperand = secondOperand * 10 + Integer.parseInt(symbol);
 
-        owner.display.setText(String.valueOf(secondOperand));
-
         memory = secondOperand;
         useMemory = false;
         useFirst = false;
+        startNew = false;
     }
 
     void setOperator(String operator) {
 
         if (firstOperand != 0) {
             calculate();
-            owner.display.setText((secondOperand + operator));
         }
 
         firstOperand = secondOperand;
@@ -101,25 +96,23 @@ public class Calculator {
         fractionalPart = 0;
 
         this.operator = operator.charAt(0);
-
-        owner.display.setText(operator);
         useMemory = true;
         useFirst = true;
+        startNew = false;
     }
 
     void signChange() {
         secondOperand = -secondOperand;
-        owner.display.setText(String.valueOf(secondOperand));
     }
 
     void sqrt() {
         secondOperand = Math.sqrt(secondOperand);
-        owner.display.setText(String.valueOf(secondOperand));
+        memory = secondOperand;
+        startNew = false;
     }
 
     void round() {
         double roundingMode = Math.pow(10, fractionalPart - 1);
         secondOperand = (int)(secondOperand * roundingMode) / roundingMode;
-
     }
 }
--- Window.java
+++ Window.java
@@ -4,19 +4,24 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
 
 public class Window extends JFrame implements ActionListener {
     Container container = getContentPane();
     FlowLayout layout = new FlowLayout(FlowLayout.LEFT, 3, 3);
     Calculator calculator = new Calculator(this);
     boolean isError;
+    final int CHARS_MAX_NUMBER = 17;
+    final double MAX_VALUE = 1.0E17;
+    final double MIN_VALUE = -1.0E17;
 
     JButton[] buttons = new JButton[20];
     String[] names =
             {
-                    "C", "" + (char)0x21D0, "" + (char)0x221A, "+", "1", "2", "3", "-", "4", "5", "6", "*", "7", "8",
-                    "9", "" + (char)0xF7, ".", "0", "+/-", "="
+                    "C", "" + (char)0x21D0, "" + (char)0x221A, "+", "1", "2", "3", "-", "4", "5", "6", "×", "7", "8",
+                    "9", "÷", ".", "0", "+/-", "="
             };
+    String operators = "-+÷×";
 
     JTextField display = new JTextField();
 
@@ -35,8 +40,8 @@ public class Window extends JFrame implements ActionListener {
         container.setLayout(layout);
         container.setBackground(backgroundColor);
 
-        for (int i = 0; i < buttons.length; i++)
-        {
+        for (int i = 0; i < buttons.length; i++) {
+
             buttons[i] = new JButton(names[i]);
             buttons[i].setPreferredSize(new Dimension(42, 30));
             buttons[i].setBackground(buttonColor);
@@ -66,44 +71,44 @@ public class Window extends JFrame implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        if (!isError)
-            switch(e.getActionCommand())
-            {
-                case "C":
-                    calculator.cancel();
-                    break;
-                case ("" + (char)0x21D0)://стирание знаков
-                    calculator.backspace();
-                    break;
-                case ("" + (char)0x221A)://извлечение корня
-                    calculator.sqrt();
-                    break;
-
-                case "+":
-                case "-":
-                case "*":
-                case ("" + (char)0xF7):
-                    calculator.setOperator(e.getActionCommand());
-                    break;
-
-                case "=":
-                    calculator.calculate();
-                    break;
-
-                case "+/-":
-                    calculator.signChange();
-                    break;
-
-                default:
-                    calculator.numberInput(e.getActionCommand());
+    public void actionPerformed(ActionEvent e) {
+        if (!isError) {
+            switch (e.getActionCommand()) {
+                case "C" -> calculator.cancel();
+                //стирание знаков
+                case ("" + (char) 0x21D0) -> calculator.backspace();
+                //извлечение корня
+                case ("" + (char) 0x221A) -> calculator.sqrt();
+                case "+", "-", "×", "÷" -> calculator.setOperator(e.getActionCommand());
+                case "=" -> calculator.calculate();
+                case "+/-" -> calculator.signChange();
+                default -> calculator.numberInput(e.getActionCommand());
             }
-        else if (e.getActionCommand().equals("C")) calculator.cancel();
+
+            if (operators.contains(e.getActionCommand())) {
+                print(e.getActionCommand());
+            } else
+                print(BigDecimal.valueOf(calculator.secondOperand).toPlainString());
+        } else if (e.getActionCommand().equals("C")) calculator.cancel();
     }
 
     void error() {
         display.setText("error");
         isError = true;
+    }
+
+    void check(double d) {
+        if (d > MAX_VALUE || d < MIN_VALUE) error();
+    }
+
+    void print(String s) {
+        if (s.length() != 1) {
+            if (calculator.fractionalPart < 1) s = s.substring(0, s.length() - 2);
+            if (calculator.fractionalPart == 1) s = s.substring(0, s.length() - 1);
+        }
+
+        if (s.length() > CHARS_MAX_NUMBER) s = s.substring(0, CHARS_MAX_NUMBER + 1);
+
+        display.setText(s);
     }
 }
